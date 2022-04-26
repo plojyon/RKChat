459dcead8c5ecf85d3d4ee6cb90a42d9e2c89511
Yon Ploj <yon.ploj@gmail.com>
2022-04-26 22:19:09 +0200

    Add basic DM functionality

--- chatClient.py
+++ chatClient.py
@@ -15,131 +15,149 @@ username = None
 
 ### MESSAGE TYPES
 
+
 def send_message(sock, message):
-	if len(message) >= 1<<14:
-		raise ValueError("Message too long")
-	encoded = encode_message(message=message, type=TYPE["public"])
-	sock.sendall(encoded)
+    if len(message) >= 1 << 14:
+        raise ValueError("Message too long")
+    encoded = encode_message(message=message, type=TYPE["public"])
+    sock.sendall(encoded)
+
 
 def send_dm(sock, message, recipient):
-	if len(message) >= 1<<14:
-		raise ValueError("Message too long")
-	if len(recipient) >= 1<<8:
-		raise ValueError("Recipient name too long")
-	encoded = encode_message(message=message, type=TYPE["private"], user=recipient)
-	sock.sendall(encoded)
+    if len(message) >= 1 << 14:
+        raise ValueError("Message too long")
+    if len(recipient) >= 1 << 8:
+        raise ValueError("Recipient name too long")
+    encoded = encode_message(message=message, type=TYPE["private"], user=recipient)
+    sock.sendall(encoded)
+
 
 def send_name(sock, username):
-	if len(username) >= 1<<8:
-		log("Username too long")
-		return change_username()
-	encoded = encode_message(type=TYPE["username"], user=username)
-	sock.sendall(encoded)
+    if len(username) >= 1 << 8:
+        log("Username too long")
+        return change_username()
+    encoded = encode_message(type=TYPE["username"], user=username)
+    sock.sendall(encoded)
+
 
 ###
 
 
 # message_receiver funkcija tece v loceni niti
 def message_receiver():
-	global connected_users
-	global chat_window
-	global participants_window
-	global canvas
+    global connected_users
+    global chat_window
+    global participants_window
+    global canvas
 
-	while True:
-		msg = receive_message(sock)
-		if msg["type"] == TYPE["username"]:
-			if msg["message"] == "0":
-				if msg["username"] in connected_users:
-					connected_users.remove(msg["username"])
-					log(msg["username"]+" disconnected")
-			elif msg["message"] == "1":
-				connected_users.append(msg["username"])
-				log(msg["username"]+" connected")
-			else:
-				# username is confirmed OK
-				set_username(msg["username"])
-			participants_window.clear()
-			for u in connected_users:
-				participants_window.print(u)
-			canvas.draw()
-		elif msg["type"] == TYPE["error"]:
-			handle_error(msg)
-		else:
-			chat_window.print(format_message(msg))
-		canvas.draw()
+    while True:
+        msg = receive_message(sock)
+        if msg["type"] == TYPE["username"]:
+            if msg["message"] == "0":
+                if msg["username"] in connected_users:
+                    connected_users.remove(msg["username"])
+                    log(msg["username"] + " disconnected")
+            elif msg["message"] == "1":
+                connected_users.append(msg["username"])
+                log(msg["username"] + " connected")
+            else:
+                # username is confirmed OK
+                set_username(msg["username"])
+            participants_window.clear()
+            for u in connected_users:
+                participants_window.print(u)
+            canvas.draw()
+        elif msg["type"] == TYPE["error"]:
+            handle_error(msg)
+        else:
+            chat_window.print(format_message(msg))
+        canvas.draw()
+
 
 def handle_error(msg):
-	if (msg["code"] == ERRORS["invalid_username"]):
-		log(msg["message"])
-		change_username()
-	if (msg["code"] == ERRORS["banned"]):
-		sys.exit(0)
+    if msg["code"] == ERRORS["invalid_username"]:
+        log(msg["message"])
+        change_username()
+    if msg["code"] == ERRORS["banned"]:
+        sys.exit(0)
+
 
 def get_input(prompt):
-	input_window.clear()
-	canvas.draw()
-	pos = input_window.position
-	pycat.cursor.move(*input_window.inner_position)
-	return input(prompt)
+    input_window.clear()
+    canvas.draw()
+    pos = input_window.position
+    pycat.cursor.move(*input_window.inner_position)
+    return input(prompt)
+
 
 def change_username():
-	username = get_input("Enter a username: ")
-	input_window.print("Logging in ...")
-	send_name(sock, username)
+    username = get_input("Enter a username: ")
+    input_window.print("Logging in ...")
+    send_name(sock, username)
+
 
 def title_text(left, center, right, available_width):
-	width_left = int(math.floor(available_width / 2.0))
-	width_right = int(math.ceil(available_width / 2.0))
-	padding_left = width_left - len(left) - int(math.floor(len(center)/2.0))
-	padding_right = width_right - len(right) - int(math.ceil(len(center)/2.0))
-	return left + padding_left*" " + center + padding_right*" " + right
+    width_left = int(math.floor(available_width / 2.0))
+    width_right = int(math.ceil(available_width / 2.0))
+    padding_left = width_left - len(left) - int(math.floor(len(center) / 2.0))
+    padding_right = width_right - len(right) - int(math.ceil(len(center) / 2.0))
+    return left + padding_left * " " + center + padding_right * " " + right
+
 
 def set_status(status):
-	available_width = status_window.size[0]-2
-	text = title_text(" " + status, "=== Welcome to RKChat ===", "_  □  X ", available_width)
-	status_window.clear()
-	status_window.print(text)
-	canvas.draw()
+    available_width = status_window.size[0] - 2
+    text = title_text(
+        " " + status, "=== Welcome to RKChat ===", "_  □  X ", available_width
+    )
+    status_window.clear()
+    status_window.print(text)
+    canvas.draw()
+
 
 def set_username(uname):
-	global username
-	log("Login successful.")
-	set_status("Logged in as "+uname)
-	username = uname
+    global username
+    log("Login successful.")
+    set_status("Logged in as " + uname)
+    username = uname
+
 
 def log(message):
-	chat_window.print(f"{format_ts(time.time())} [LOG] {message}")
-	canvas.draw()
+    chat_window.print(f"{format_ts(time.time())} [LOG] {message}")
+    canvas.draw()
+
 
 sidebar_width = 30
 connected_users = []
 
-w,h = pycat.cursor.get_terminal_size()
-status_window = pycat.Window(position=(0, 0), size=(w, 3))
-chat_window = pycat.windows.ConsoleWindow(position=(0, 2), size=(w-sidebar_width+1,h-4), padding=[1, 2, 1, 2])
-participants_window = pycat.windows.ListWindow(position=(w-30, 2), size=(sidebar_width,h), padding=[1, 2, 1, 2])
-input_window = pycat.windows.InputWindow(position=(0, h-3), size=(w, 3), padding=[1,1,1,2])
+w, h = pycat.cursor.get_terminal_size()
+status_window = pycat.Window(position=(0, 0), size=(w, 3), padding=[1, 1, 1, 1])
+chat_window = pycat.windows.ConsoleWindow(
+    position=(0, 2), size=(w - sidebar_width + 1, h - 4)
+)
+participants_window = pycat.windows.ListWindow(
+    position=(w - 30, 2), size=(sidebar_width, h - 4)
+)
+input_window = pycat.windows.InputWindow(position=(0, h - 3), size=(w, 3))
 canvas = pycat.Canvas([chat_window, participants_window, status_window, input_window])
 
 canvas.draw()
 
 set_status("Connecting ...")
-log("Connecting to "+str(SERVER))
+log("Connecting to " + str(SERVER))
 
 while True:
-	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect(SERVER)
-	except Exception as e:
-		set_status("Connection error")
-		log(str(e))
-		get_input("Press Enter to attempt to reconnect or Ctrl-C to quit.")
-		log("Reconnecting ...")
-		set_status("Connecting ...")
-		continue
-	break;
-set_status("Connected to "+str(SERVER))
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(SERVER)
+    except Exception as e:
+        set_status("Connection error")
+        log(str(e))
+        get_input("Press Enter to attempt to reconnect or Ctrl-C to quit.")
+        log("Reconnecting ...")
+        set_status("Connecting ...")
+        continue
+    break
+set_status("Connected to " + str(SERVER))
 log("Connected.")
 
 # begin receiving messages
@@ -151,13 +169,20 @@ thread.start()
 change_username()
 
 while username is None:
-	time.sleep(0.1)
+    time.sleep(0.1)
 
 # await input
 while True:
-	try:
-		time.sleep(0.1)
-		msg_send = get_input("> ")
-		send_message(sock, msg_send)
-	except KeyboardInterrupt:
-		sys.exit(0)
+    try:
+        time.sleep(0.1)
+        msg_send = get_input("> ")
+        if msg_send.startswith("/dm "):
+            args = msg_send.split()
+            if len(args) >= 3:
+                recipient = args[1]
+                message = " ".join(args[2:])
+                send_dm(sock, message, recipient)
+                continue
+        send_message(sock, msg_send)
+    except KeyboardInterrupt:
+        sys.exit(0)
--- chatServer.py
+++ chatServer.py
@@ -10,101 +10,134 @@ from common import *
 sock_to_uname = {}
 uname_to_sock = {}
 
+
 def add_user(socket, username):
-	if socket in sock_to_uname:
-		socket.send(encode_message(message="User already has a name", type=TYPE["error"], code=ERRORS["invalid_username"]))
-		return False
-	if username in uname_to_sock:
-		socket.send(encode_message(message="Username is taken", type=TYPE["error"], code=ERRORS["invalid_username"]))
-		return False
+    if socket in sock_to_uname:
+        socket.send(
+            encode_message(
+                message="User already has a name",
+                type=TYPE["error"],
+                code=ERRORS["invalid_username"],
+            )
+        )
+        return False
+    if username in uname_to_sock:
+        socket.send(
+            encode_message(
+                message="Username is taken",
+                type=TYPE["error"],
+                code=ERRORS["invalid_username"],
+            )
+        )
+        return False
 
-	# confirm username with client
-	try:
-		socket.send(encode_message(message="2", type=TYPE["username"], user=username))
-	except:
-		pass
+    # confirm username with client
+    try:
+        socket.send(encode_message(message="2", type=TYPE["username"], user=username))
+    except:
+        pass
 
-	# broadcast new client to everyone
-	for client in clients:
-		try:
-			client.send(encode_message(message="1", type=TYPE["username"], user=username))
-		except:
-			pass
+    # broadcast new client to everyone
+    for client in clients:
+        try:
+            client.send(
+                encode_message(message="1", type=TYPE["username"], user=username)
+            )
+        except:
+            pass
 
-	# notify new client of all other logged-in users
-	for user in uname_to_sock:
-		try:
-			client_sock.send(encode_message(message="1", type=TYPE["username"], user=user))
-		except:
-			pass
+    # notify new client of all other logged-in users
+    for user in uname_to_sock:
+        try:
+            client_sock.send(
+                encode_message(message="1", type=TYPE["username"], user=user)
+            )
+        except:
+            pass
 
-	sock_to_uname[socket] = username
-	uname_to_sock[username] = socket
+    sock_to_uname[socket] = username
+    uname_to_sock[username] = socket
 
-	return True
+    return True
+
 
 def remove_user(socket):
-	username = sock_to_uname[socket]
+    username = sock_to_uname[socket]
 
-	# broadcast client loss
-	for client in set(clients) - set([socket]):
-		try:
-			client.send(encode_message(message="0", type=TYPE["username"], user=username))
-		except:
-			pass
+    # broadcast client loss
+    for client in set(clients) - set([socket]):
+        try:
+            client.send(
+                encode_message(message="0", type=TYPE["username"], user=username)
+            )
+        except:
+            pass
 
-	sock_to_uname.pop(socket, None)
-	uname_to_sock.pop(username, None)
+    sock_to_uname.pop(socket, None)
+    uname_to_sock.pop(username, None)
 
 
 # funkcija za komunikacijo z odjemalcem (tece v loceni niti za vsakega odjemalca)
 def client_thread(client_sock, client_addr):
-	global clients
-	global usernames
+    global clients
+    global usernames
 
-	print("[system] connected with " + client_addr[0] + ":" + str(client_addr[1]))
-	print("[system] we now have " + str(len(clients)) + " clients")
+    print("[system] connected with " + client_addr[0] + ":" + str(client_addr[1]))
+    print("[system] we now have " + str(len(clients)) + " clients")
 
-	try:
-		while True:
-			msg = receive_message(client_sock)
-			if msg["type"] == TYPE["username"]:
-				add_user(client_sock, msg["username"])
-				continue
-			if msg["type"] == TYPE["error"]:
-				continue
-			if msg["message"] is None or len(msg["message"]) == 0:
-				continue
+    try:
+        while True:
+            msg = receive_message(client_sock)
+            if msg["type"] == TYPE["username"]:
+                add_user(client_sock, msg["username"])
+                continue
+            if msg["type"] == TYPE["error"]:
+                continue
+            if msg["message"] is None or len(msg["message"]) == 0:
+                continue
 
-			msg["username"] = sock_to_uname[client_sock]
-			msg["code"] = len(sock_to_uname[client_sock])
+            # determine set of intended recipients
+            recipients = clients
+            if msg["type"] == TYPE["private"]:
+                recipients = [uname_to_sock[msg["username"]], client_sock]
 
-			message = format_message(msg)
-			print("[{ip}:{port}] {message}".format(
-				ip=client_addr[0],
-				port=client_addr[1],
-				message=message,
-			))
+            # format message for logging
+            msg["username"] = sock_to_uname[client_sock]
+            msg["code"] = len(sock_to_uname[client_sock])
+            message = format_message(msg)
+            print(
+                "[{ip}:{port}] {message}".format(
+                    ip=client_addr[0],
+                    port=client_addr[1],
+                    message=message,
+                )
+            )
 
-			# determine set of intended recipients
-			recipients = clients
-			if msg["type"] == TYPE["private"]:
-				recipients = [usernames[msg["username"]]]
+            # send message to intended recipients
+            for client in recipients:
+                try:
+                    client.send(
+                        encode_message(
+                            message=msg["message"],
+                            type=msg["type"],
+                            user=sock_to_uname[client_sock],
+                        )
+                    )
+                except BrokenPipeError:
+                    pass
+    except ConnectionResetError as e:
+        print(e)
+        print("Deleting client ...")
+        remove_user(client_sock)
 
-			# send message to intended recipients
-			for client in recipients:
-				client.send(encode_message(message=msg["message"], type=msg["type"], user=sock_to_uname[client_sock]))
-	except ConnectionResetError as e:
-		print(e)
-		print("Deleting client ...")
-		remove_user(client_sock)
+    with clients_lock:
+        clients.remove(client_sock)
+    print("[system] we now have " + str(len(clients)) + " clients")
+    client_sock.close()
 
-	with clients_lock:
-		clients.remove(client_sock)
-	print("[system] we now have " + str(len(clients)) + " clients")
-	client_sock.close()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("localhost", PORT))
 server_socket.listen(1)
 
@@ -112,18 +145,18 @@ print("[system] listening ...")
 clients = set()
 clients_lock = threading.Lock()
 while True:
-	try:
-		# pocakaj na novo povezavo - blokirajoc klic
-		client_sock, client_addr = server_socket.accept()
-		with clients_lock:
-			clients.add(client_sock)
+    try:
+        # pocakaj na novo povezavo - blokirajoc klic
+        client_sock, client_addr = server_socket.accept()
+        with clients_lock:
+            clients.add(client_sock)
 
-		thread = threading.Thread(target=client_thread, args=(client_sock, client_addr));
-		thread.daemon = True
-		thread.start()
+        thread = threading.Thread(target=client_thread, args=(client_sock, client_addr))
+        thread.daemon = True
+        thread.start()
 
-	except KeyboardInterrupt:
-		break
+    except KeyboardInterrupt:
+        break
 
 print("[system] closing server socket ...")
 server_socket.close()
